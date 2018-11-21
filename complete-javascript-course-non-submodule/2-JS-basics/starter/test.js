1a8ddc40f56c357558dac43f3eac633eaa5209cf
Jonathan Welch <jonathanwelch@notonthehighstreet.com>
2018-11-21 11:02:19 +0000

    Remove submodule mapping

---
+++ complete-javascript-course-non-submodule/2-JS-basics/starter/test.js
@@ -0,0 +1,34 @@
+
+console.log("Jon's Demo Test");
+
+var firstName = "Jon";
+var lastName = "Welch"
+
+var age = "28";
+
+//age = 'twenty eight';
+
+var job = "QA";
+
+console.log(firstName +' '+ lastName + ' is a '+ age +' year old '+ job);
+
+alert(firstName +' '+ lastName + ' is a '+ age +' year old '+ job);
+
+var middleName =prompt("What is his middle name?")
+console.log(firstName +' '+ middleName +' '+ lastName)
+
+var yobJon , yobMeg;
+
+var ageMeg = "22";
+
+now = "2018";
+
+yobJon = (now - age) -1;
+yobMeg = now - ageMeg;
+
+console.log("Jon was born in" +' '+ yobJon)
+console.log("Meg was born in" +' '+ yobMeg)
+
+var jonOlder = age > ageMeg;
+console.log(jonOlder)
+
