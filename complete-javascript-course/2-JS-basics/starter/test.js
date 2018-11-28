80bc2b51c6ef773d8db1ec0b1811a7800d8751cf
Jonathan Welch <jonathanwelch@notonthehighstreet.com>
2018-11-28 16:08:51 +0000

    learning if else statements

--- complete-javascript-course/2-JS-basics/starter/test.js
+++ complete-javascript-course/2-JS-basics/starter/test.js
@@ -4,7 +4,7 @@ console.log("Jon's Demo Test");
 var firstName = "Jon";
 var lastName = "Welch"
 
-var age = "28";
+var age = "30";
 
 //age = 'twenty eight';
 
@@ -14,14 +14,15 @@ console.log(firstName +' '+ lastName + ' is a '+ age +' year old '+ job);
 
 alert(firstName +' '+ lastName + ' is a '+ age +' year old '+ job);
 
-var middleName =prompt("What is his middle name?")
+var middleName = prompt("What is his middle name?")
 console.log(firstName +' '+ middleName +' '+ lastName)
 
 var yobJon , yobMeg;
 
-var ageMeg = "22";
-
 now = "2018";
+
+var ageMeg = "22";
+var ageJoeBloggs = "35";
 
 yobJon = (now - age) -1;
 yobMeg = now - ageMeg;
@@ -31,4 +32,23 @@ console.log("Meg was born in" +' '+ yobMeg)
 
 var jonOlder = age > ageMeg;
 console.log(jonOlder)
+
+if (jonOlder === true){
+    console.log("Jon is older than Meg");
+}
+else{
+    console.log("Meg is older than Jon");
+}
+
+
+if (age >= 28 && age <30 ){
+    console.log("This is the age of" +' '+ firstName);
+}
+else if (age >=22 && age <28 ){
+    console.log("This is the age of Meg");
+}
+else {
+    console.log("This is the age of Joe Bloggs");
+}
+    
 
